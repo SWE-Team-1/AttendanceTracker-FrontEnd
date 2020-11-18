256e4c7dc1ac1b90fb320bfd28a643e2b56a5951
T-Camp <tcampbe6@unb.ca>
2020-11-18 19:00:36 -0400

    Adding some logic to createAccount button
    Added logic which forces the fields when creating an account to have value. The minimum length of each field if specified with a constant.
    Also made it so professor view is now default

--- attendance_tracker_app/src/CreateAccount.js
+++ attendance_tracker_app/src/CreateAccount.js
@@ -3,23 +3,51 @@ import './CreateAccount.css'
 import loginImage from './graphic_assets/Login_Image.svg'
 import { Redirect } from 'react-router-dom'
 
+const MIN_NAME_LENGTH = 1
+const MIN_EMAIL_LENGTH = 5
+const MIN_PASSWORD_LENGTH = 8
+
 class CreateAccount extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       error: null,
-      prof: false
+      prof: true,
+      created: false
     }
     // This page should be a form that sends all feild's value to the login function on submit
   }
 
   tryCreateAccount (tryName, tryEmail, tryPassword) {
     // Secure account creation with backend information saving if valid, for now does nothing
-    if (this.state.prof) {
-      //Check input values for prof account creation validation
-    } else {
+    if (tryName.length < MIN_NAME_LENGTH){
+      this.setState({ error: "Name not filled in properly"})
+    } 
+    else if (tryEmail.length < MIN_EMAIL_LENGTH){
+      this.setState({ error: "Email not filled in properly"})
+    }
+    else if (tryPassword.length < MIN_PASSWORD_LENGTH) {
+      this.setState({ error: "Password not filled in properly"})       
+    }
+    else {
+      if (this.state.prof) {
+        //Check input values for prof account creation validation
+
+        //if CreateProfAccount is successfull
+          this.setState({created: true})
+        //else
+          this.setState({ error: "Cannot create professor account"})
+
+      }
+      else {
       //Check input values for student account creation validation
-      this.setState({ error: 'Invalid account creation'})
+      
+      //if CreateProfAccount is successfull
+        this.setState({created: true})
+      //else
+        this.setState({ error: "Cannot create student account"})
+
+      }
     }
   }
 
@@ -39,10 +67,10 @@ class CreateAccount extends React.Component {
             <div />
             <input type='password' id='accountPassword' name='accountPassword' placeholder='Password' />
             <div />
-            {this.state.prof ? null : <input type='text' id='accountOneTimeKey' name='accountoneTimeKey' placeholder='One-time Security Key' />}
+            {this.state.prof ? null : <input type='text' id='accountOneTimeKey' name='accountOneTimeKey' placeholder='One-time Security Key' />}
             <div />
             <button className='CreateAccount-Button' onClick={() => this.tryCreateAccount(document.getElementById("accountName").value, document.getElementById("accountEmail").value, document.getElementById("accountPassword").value)}>Create Account</button>
-
+            {this.state.created ? <Redirect to='/' /> : null}
             <br />
             <a href='/login' className='CreateAccount-backToLogin'>Back To Login</a>
 

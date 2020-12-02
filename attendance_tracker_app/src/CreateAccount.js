8d9edeae99cf11e3a7de605a0fbe806a38b66dff
T-Camp <tcampbe6@unb.ca>
2020-12-02 12:22:59 -0400

    Commit for backend requests
    I have written code to receive and respond properly to creating an account (including the routing to '/{userID}' ). Final testing needs to be done as my backend only responds with errors (has been discussed with Jacob).
    Also, currently if creating the account fails, it only sets the error state to "Failed Create Account" as I am unsure of what happens if it errors.

--- attendance_tracker_app/src/CreateAccount.js
+++ attendance_tracker_app/src/CreateAccount.js
@@ -3,7 +3,7 @@ import './CreateAccount.css'
 import loginImage from './graphic_assets/Login_Image.svg'
 import { Redirect } from 'react-router-dom'
 
-//Constant values are currently arbitrary
+//Constant values are currently arbitrary values to ensure fields are filled when creating account
 const MIN_NAME_LENGTH = 1
 const MIN_EMAIL_LENGTH = 5
 const MIN_PASSWORD_LENGTH = 8
@@ -16,14 +16,14 @@ class CreateAccount extends React.Component {
       back: false,
       prof: true,
       created: false,
-      id: null
+      id: null //preset to invalid ID, to be used in routing
     }
     // This page should be a form that sends all feild's value to the login function on submit
   }
 
   tryCreateAccount (tryName, tryEmail, tryPassword) {
     // Secure account creation with backend information saving if valid
-    
+
     //Only allows properly filled out fields to be analyzed
     if (tryName.length < MIN_NAME_LENGTH) {
       this.setState({ error: 'Name not filled in properly' })
@@ -37,19 +37,26 @@ class CreateAccount extends React.Component {
       var xhr = new XMLHttpRequest()
 
       xhr.addEventListener('load', () => {
+        var response = JSON.parse(xhr.responseText)
+        
         //If there is an error in creating account, display
-        document.getElementById('CreateAccount-Result').value = xhr.responseText
+        if (response.id === undefined) {
+          this.setState({ error: "Failed create account"})
+        }
         //Else, should redirect to logged in screen
-        this.setState({ created: true, id: xhr.responseText })
+        else {
+          this.setState({ created: true, id: '/' + response.id}) //Set id to include the routing for easier redirection
+        }
       })
 
       if (this.state.prof) {
         //Attempt Create a professor account
-        xhr.open('POST', "http://ats@192.168.56.101/user/create/email/" + tryEmail + "/password/" + tryPassword + "/type/" + 2)
+        xhr.open('POST', "http://localhost:8080/user/create/email/" + tryEmail + "/password/" + tryPassword + "/type/" + 2)
+        
       }
       else {
         //Attempt Create a student account
-        xhr.open('POST', "http://ats@192.168.56.101/user/create/email/" + tryEmail + "/password/" + tryPassword + "/type/" + 1)
+        xhr.open('POST', "http://localhost:8080/user/create/email/" + tryEmail + "/password/" + tryPassword + "/type/" + 1)
       }
       xhr.send()
     }
@@ -74,12 +81,11 @@ class CreateAccount extends React.Component {
             {this.state.prof ? null : <input type='text' id='accountOneTimeKey' name='accountOneTimeKey' placeholder='One-time Security Key' />} {/*Specific for student account creation*/}
             <div />
             <button className='CreateAccount-Button' onClick={() => this.tryCreateAccount(document.getElementById('accountName').value, document.getElementById('accountEmail').value, document.getElementById('accountPassword').value)}>Create Account</button>
-            {this.state.created ? <Redirect to='/' /> : null}
+            {this.state.created ? <Redirect to={this.state.id} /> : null}
             <br />
             <div className='CreateAccount-backToLogin' onClick={() => this.setState({ back: !this.state.back })}>Back To Login</div>
             {this.state.back ? <Redirect to='/login' /> : null}
             <div />
-            <h5 classname='CreateAccount-Result' id='CreateAccount-Result' name='CreateAccount-Result' ></h5>
 
           </div>
         </div>

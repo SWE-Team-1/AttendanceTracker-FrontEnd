ca32daf63940e731de141235d56feaaa04e85b03
Robert Stepner <rstepner@unb.ca>
2020-12-08 12:15:05 -0400

    Applied routing to Morgan's work (buttons)

--- attendance_tracker_app/src/View.js
+++ attendance_tracker_app/src/View.js
@@ -1,68 +1,53 @@
 import React from 'react'
 import './View.css'
-import PopUpMenu from './PopUpMenu.js'
+import { Redirect } from 'react-router-dom'
 
 class View extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      show: false
-      //states: false
+      logout : false,
+      redirectOption: 'lecture',
+      courses: [
+        'CS3113',
+        'CS2413',
+        'CS3413',
+        'TME4125',
+        'SWE4102'
+      ]
     }
   }
-
- /* toggleMenu = () => {
-    this.setState({show: !this.state.show});
-  }*/
 
   render () {
     return (
       <div className='View'>
         <div classname='View-Header'>
-          <div className='Header-Text'>ATTENDANCE TRACKER ({this.props.prof ? 'Professor' : 'Student'})</div>
-          <div className='Logout'><button onClick={() => this.props.logout()}>Logout</button></div>
+          <div className='Header-Text'>ATTENDANCE TRACKER</div>
+          <div className='Logout'> <button onClick={() => this.setState({ logout: true })}>Logout</button>
+            {this.state.logout ? <Redirect to='/login' /> : null}
+          </div>
         </div>
 
         <div className='View-Container'>
           <h5 className='View-Title-Courses'>Your Courses</h5>
           <div className='View-Total-Frame'>
-
-            <a className='View-Courses'>
-              <div className="Header-Text">SWE4103</div>
-              <button href="#">LECTURE</button>
-              <button href="#">TUTORIAL</button>
-              <button href="#">LAB</button>
-            </a>
-            
-            <a className='View-Courses'>
-              <div className="Header-Text">CS3413</div>
-              <button href="#">LECTURE</button>
-              <button href="#">LAB</button>
-            </a>
-
-            <a className='View-Courses'>
-              <div className="Header-Text">CS3383</div>
-              <button href="#">LECTURE</button>
-              <button href="#">TUTORIAL</button>
-            </a>
-
-            <a className='View-Courses'>
-              <div className="Header-Text">ENGG4013</div>
-              <button href="#">LECTURE</button>
-            </a>
-
-            <a className='View-Courses'>
-              <div className="Header-Text">SWE4040</div>
-              <button href="#">LECTURE</button>
-            </a>
-            
-          </div>
+            {this.state.courses.map(course =>
+              <div className='View-Courses' key={course}>
+                {this.state.login ? <Redirect to={'/' + course + '/' + this.state.redirectOption} /> : null}
+                <h3 className='Header-Text'>{course}</h3>
+                  <button onClick={() => this.setState({ redirectOption: 'lecture', login: true })}>Lecture</button>
+                  <button onClick={() => this.setState({ redirectOption: 'lab', login: true })}>Lab</button>
+                  <button onClick={() => this.setState({ redirectOption: 'tutorial', login: true })}>Tutorial</button>
+              </div>
+            )}
+          </div>            
+          
           <div className='View-Area-Button View-Edit-Course'>
             <a href='#' className='View-Edit-Button View-Add-Course'>ADD NEW COURSE</a>
             <a href='#' className='View-Edit-Button View-Remove-Course'>REMOVE COURSE</a>
           </div>
         </div>
-      </div>
+        </div>
     )
   }
 }
